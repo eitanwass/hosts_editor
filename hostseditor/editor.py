3e849415a17b41fc1749b40c6e056b1a1f4c9b48
eitanwass-laptop <eitanwass@gmail.com>
2020-12-23 14:15:02 +0200

    Added documentation and minor changes to entry has api

--- hostseditor/editor.py
+++ hostseditor/editor.py
@@ -7,7 +7,14 @@ from hostseditor.utils import get_hosts_file_path, get_hosts_file_backup_path
 
 
 class HostsEditor:
+    """ An editor for hosts files. """
     def __init__(self, path: str = None, create_backup: bool = True):
+        """
+        Creates an editor for the hosts file.
+
+        :param path: The path to the system's hosts file. Default None will find automatically.
+        :param create_backup: Whether to backup the hosts file before any changes made. SUGGESTED.
+        """
         self.path = Path(path) if path else get_hosts_file_path()
         self.path.touch(exist_ok=True)
 
@@ -18,6 +25,14 @@ class HostsEditor:
             self.create_backup()
 
     def create_backup(self, physical: bool = True, memory: bool = True):
+        """
+        Back up the current state of the hosts file to another file or to memory.
+
+
+        :param physical: Whether to back up the file to another backup file.
+        :param memory: Whether to back up the file to memory.
+        :raises ValueError: If both options are disabled (because then no backup will occur).
+        """
         if not physical and not memory:
             raise ValueError(f"create_backup called but both backup methods are disabled")
         if physical:
@@ -29,11 +44,21 @@ class HostsEditor:
 
     # **** Read **** #
 
-    def read_raw(self):
+    def read_raw(self) -> str:
+        """
+        Read the data straight from the hosts file.
+
+        :return: The data in the hosts file as text.
+        """
         return Path(self.path).read_text()
 
     # TODO: Add param to parse comments as well.
     def read(self) -> typing.List[HostEntry]:
+        """
+        Read and parse hosts file entries.
+
+        :return: A list of that kind of HostEntry objects representing the hosts file entries.
+        """
         return [HostEntry.parse(line)
                 for line in self.read_raw().splitlines()
                 if line.strip() and not HostEntry.is_comment(line)]
@@ -42,11 +67,18 @@ class HostsEditor:
 
     # TODO: Add function for where one of the names has to exist to delete entry
 
-    def remove_entry_where(self, **kwargs):
+    def remove_entry_where(self, ip: str = None, names: typing.Union[str, typing.List[str]] = None) -> None:
+        """
+        Remove an entry where these attributes apply completely (not partially).
+
+        :param ip: The IP of the entry to delete.
+        :param names: The names of the entry to delete.
+        """
+        # TODO: Return bool based on success of deleting (say, if entry does not exist return False).
         lines = self.read_raw().splitlines()
 
         lines = [line for line in lines
-                 if HostEntry.is_comment(line) or not line.strip() or not HostEntry.parse(line).has(**kwargs)]
+                 if HostEntry.is_comment(line) or not line.strip() or not HostEntry.parse(line).has(ip=ip, names=names)]
 
         with self.path.open('w') as f:
             f.write('\n'.join(lines))
--- hostseditor/entry.py
+++ hostseditor/entry.py
@@ -5,28 +5,49 @@ COMMENT_CHAR = "#"
 
 
 class HostEntry:
-    def __init__(self, ip: str, names: typing.List[str]):
+    """ An object representing an entry in the relevant hosts file. """
+    def __init__(self, ip: str, names: typing.Union[str, typing.List[str]]):
+        """
+        An object representing an entry in the relevant hosts file.
+
+        :param ip: The ip of the entry.
+        :param names: The names of the hosts that this IP relates to.
+        """
         self.ip = ip
         self.names = names
 
     @staticmethod
-    def parse(line: str) -> typing.Union[HostEntry]:
+    def parse(line: str) -> HostEntry:
+        """
+        Parse a line into an HostEntry object.
+
+        :param line: The line in the hosts file.
+        :return: The HostEntry object with the attributes of that line.
+        """
         line_parts = line.split()
         return HostEntry(line_parts[0], line_parts[1:])
 
-    def has(self, **kwargs) -> bool:
-        ip_filter = kwargs.get('ip')
-        names_filter = kwargs.get('names')
+    def has(self, ip: str = None, names: typing.Union[str, typing.List[str]] = None) -> bool:
+        """
+        Does the entry have the attributes provided.
+        Acceptable attributes keys are: ip, names.
 
-        if ip_filter and self.ip != ip_filter:
+        :param ip: The IP to check if the object has.
+        :param names: The names to check if the object has.
+        :return: Whether the object has the provided attributes.
+        """
+
+        if ip and self.ip != ip:
             return False
-        if names_filter and self.names != names_filter:
+        if names and self.names != names:
             return False
         return True
 
     @staticmethod
     def is_comment(line):
+        """ Whether the provided line is a comment. """
         return line.strip().startswith(COMMENT_CHAR)
 
     def __repr__(self):
+        """ Print the object nicely as it would in the hosts file. """
         return f"{self.ip}\t{' '.join(self.names)}"
